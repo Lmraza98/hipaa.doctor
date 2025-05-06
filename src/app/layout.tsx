2ac1d38a8fe889aee4ab93af8a176ffc79b72c8e
Lucas Raza <Lmraza98@gmail.com>
2025-05-05 22:29:51 -0400

    added dotenv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID ?? ""} />
       </body>
     </html>
   );
